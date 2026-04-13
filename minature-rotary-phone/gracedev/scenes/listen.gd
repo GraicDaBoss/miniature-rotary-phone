7f0e621ec8ed362db1a5864503392b473ed8a352
GraicDaBoss <grace1gallagher@icloud.com>
2026-04-13 23:13:52 +0100

    body pulsin to energy

--- minature-rotary-phone/gracedev/scenes/listen.gd
+++ minature-rotary-phone/gracedev/scenes/listen.gd
@@ -9,12 +9,21 @@ func _enter():
 	listen_duration = lerp(3.0, 1.2, brain.energy)
 	listen_timer = 0.0
 	print("Listening... duration: ", listen_duration)
+	var spine = state_machine.boid.get_node("SpineAnimator")
+	spine.damping = 3.0   # looser, dreamier
+	spine.angular_damping = 8.0
+
+func _exit():
+	var spine = state_machine.boid.get_node("SpineAnimator")
+	spine.damping = 7.0   # back to default
+	spine.angular_damping = 20.0
 
 func _think():
 	listen_timer += get_process_delta_time()
 
 	#chilling duirng the music 
 	state_machine.boid.steering_force *= 0.1
+	
 
 	if listen_timer >= listen_duration:
 		var brain = state_machine.boid.get_node("Brain")
